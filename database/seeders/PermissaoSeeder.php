e6ecc687da942ee1c9907d9fded6b27d565605ca
diegomaustem <diego.maustem@gmail.com>
2025-09-10 10:36:59 -0300

    :recycle: refactor: Ajuste seeder permissão

--- database/seeders/PermissaoSeeder.php
+++ database/seeders/PermissaoSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use App\Models\Permissao;
 use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class PermissaoSeeder extends Seeder
 {
