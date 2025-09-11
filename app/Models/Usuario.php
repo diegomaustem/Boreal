cd3da4520bc02ff7e78647d54d3bc76112f9413d
diegomaustem <diego.maustem@gmail.com>
2025-09-11 12:11:01 -0300

    :sparkles: feat: Criando configurações para jwt do usuário

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -3,23 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class Usuario extends Model
+class Usuario extends Authenticatable implements JWTSubject
 {
     use HasFactory;
 
     public $timestamps = false;
-
     public $incrementing = false;
 
     protected $fillable = [
+        'id',
         'nome',
         'email',
         'telefone',
         'senha',
+        'criado_em',
+        'atualizado_em',
         'permissao_id'
     ];
 
@@ -32,6 +35,23 @@ class Usuario extends Model
         'atualizado_em' => 'datetime',
     ];
 
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [
+            'usuario' => [
+                'id' => $this->id,
+                'nome' => $this->nome,
+                'email' => $this->email,
+                'permissao_id' => $this->permissao_id
+            ]
+        ];
+    }
+
     public function permissao(): BelongsTo
     {
         return $this->belongsTo(Permissao::class, 'permissao_id');
@@ -40,5 +60,10 @@ class Usuario extends Model
     public function agendamentos(): HasMany
     {
         return $this->hasMany(Agendamento::class);
+    }
+
+    public function getAuthPassword()
+    {
+        return $this->senha;
     }
 }
