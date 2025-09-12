6b213c20fa0e4547caef56f7c62ee0b834f24516
diegomaustem <diego.maustem@gmail.com>
2025-09-12 12:48:12 -0300

    :wrench: chore: Configuração de primeiros sets de injeção de dependencia na aplicação

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,21 +2,23 @@
 
 namespace App\Providers;
 
+use App\Repositories\Contracts\UsuarioRepositoryInterface;
+use App\Repositories\UsuarioRepository;
+use App\Services\Auth\AuthServiceInterface;
+use App\Services\Auth\JwtAuthService;
+use App\Services\Usuario\UsuarioService;
+use App\Services\Usuario\UsuarioServiceInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
-        //
+        $this->app->bind(UsuarioRepositoryInterface::class, UsuarioRepository::class);
+        $this->app->bind(AuthServiceInterface::class, JwtAuthService::class);
+        $this->app->bind(UsuarioServiceInterface::class, UsuarioService::class);
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         //
